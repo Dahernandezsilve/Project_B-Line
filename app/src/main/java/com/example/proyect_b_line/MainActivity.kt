b99fa93b75794e3ffb37761ac12537c8b2336fdd
danval <danarvare2003@gmail.com>
2022-08-26 08:12:18 -0600

    Inicio animacion SplashScreen

--- app/src/main/java/com/example/proyect_b_line/MainActivity.kt
+++ app/src/main/java/com/example/proyect_b_line/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.proyect_b_line
 
+
+import androidx.compose.ui.graphics.Color
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
@@ -13,11 +16,18 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
 import com.example.proyect_b_line.ui.theme.Proyect_BLineTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -69,7 +79,53 @@ fun animationStart(){
         )
     }
 }
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController,
+        startDestination = "splash_screen") {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
 
+        // Main Screen
+        composable("main_screen") {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "Main Screen", color = Color.Black, fontSize = 24.sp)
+            }
+        }
+    }
+}
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    // Animation
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.7f,
+            // tween Animation
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                }))
+        // Customize the delay time
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+
+    // Image
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()) {
+        // Change the logo
+        Image(painter = painterResource(id = R.drawable.b_line_logo),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value))
+    }
+}
 
 
 @Composable
@@ -89,7 +145,7 @@ fun DefaultPreview() {
             Column(modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                animationStart()
+                Navigation()
             }
         }
     }
