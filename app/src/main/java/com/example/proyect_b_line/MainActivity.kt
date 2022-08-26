d41098e387c1cc725a6d9b15615ab83c08ba9f00
danval <danarvare2003@gmail.com>
2022-08-25 22:29:11 -0600

    Principio de la animacion

--- app/src/main/java/com/example/proyect_b_line/MainActivity.kt
+++ app/src/main/java/com/example/proyect_b_line/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.proyect_b_line
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.*
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.estimateAnimationDurationMillis
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.proyect_b_line.ui.theme.Proyect_BLineTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,6 +30,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Scaffold() {
+
+                    }
                     Greeting("Android")
                 }
             }
@@ -29,15 +40,57 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun animationStart(){
+    // Create a MutableTransitionState<Boolean> for the AnimatedVisibility.
+    val state = remember {
+        MutableTransitionState(false).apply {
+            // Start the animation immediately.
+            targetState = true
+        }
+
+    }
+    
+    Column(){
+        AnimatedVisibility(visibleState = state) {
+            Image(painter = painterResource(id = R.drawable.b_line_logo), contentDescription ="lll" )
+        }
+
+        // Use the MutableTransitionState to know the current animation state
+        // of the AnimatedVisibility.
+        Text(
+            text = when {
+                state.isIdle && state.currentState -> "Visible"
+                !state.isIdle && state.currentState -> "Disappearing"
+                state.isIdle && !state.currentState -> "Invisible"
+                else -> "Appearing"
+            }
+        )
+    }
+}
+
+
+
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Proyect_BLineTheme {
-        Greeting("Android")
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Scaffold() {
+            Column(modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                animationStart()
+            }
+        }
     }
 }
