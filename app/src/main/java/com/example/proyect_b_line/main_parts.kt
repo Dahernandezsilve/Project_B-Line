9ddc971a4f7929b412893af3b70db7c48d8ac103
danval <danarvare2003@gmail.com>
2022-09-07 09:25:32 -0600

    Se agregaron los cambios a las cards de los productos y se establecieron bordes para estas

--- app/src/main/java/com/example/proyect_b_line/main_parts.kt
+++ app/src/main/java/com/example/proyect_b_line/main_parts.kt
@@ -2,24 +2,36 @@ package com.example.proyect_b_line
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import androidx.compose.material.Card
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun productCart(url: String){
     Card(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(15.dp)
-            .clickable{ },
-        elevation = 10.dp
+            .clickable { }
+            .size(150.dp),
+        elevation = 10.dp,
+        shape = RoundedCornerShape(15.dp)
     ){
-    Column() {
-            AsyncImage(model = url, contentDescription = "Hola texto")
+
+    Column(verticalArrangement = Arrangement.Center, modifier = Modifier.padding(12.dp)){
+            AsyncImage(model = url, contentDescription = "Hola texto", Modifier.requiredSize(125.dp))
         }
 
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardPreview() {
+    productCart(url = "https://www.steren.com.gt/media/catalog/product/cache/b69086f136192bea7a4d681a8eaf533d/image/20986abca/audifonos-bluetooth-con-bateria-de-hasta-30-h.jpg")
 }
