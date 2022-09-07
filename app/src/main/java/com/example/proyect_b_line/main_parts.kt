71c973c642430072576973830093d970704e488c
danval <danarvare2003@gmail.com>
2022-09-07 08:31:28 -0600

    Se agrego una version preliminar de las cartas en que se muestran los productos

--- app/src/main/java/com/example/proyect_b_line/main_parts.kt
+++ app/src/main/java/com/example/proyect_b_line/main_parts.kt
@@ -1,13 +1,25 @@
 package com.example.proyect_b_line
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import androidx.compose.material.Card
+import androidx.compose.ui.Modifier
 
 @Composable
 fun productCart(url: String){
-    Box{
-        AsyncImage(model = url, contentDescription = "Hola texto")
+    Card(
+        modifier = Modifier.fillMaxWidth()
+            .padding(15.dp)
+            .clickable{ },
+        elevation = 10.dp
+    ){
+    Column() {
+            AsyncImage(model = url, contentDescription = "Hola texto")
+        }
+
     }
 
 }
