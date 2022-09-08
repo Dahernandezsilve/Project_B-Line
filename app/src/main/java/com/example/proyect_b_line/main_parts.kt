3b6261445bc0f17229231b5d34aabffa8edf4231
danval <danarvare2003@gmail.com>
2022-09-08 00:37:15 -0600

    Se agraga parte de la carta de los prodcutos como el boton y parte del texto

--- app/src/main/java/com/example/proyect_b_line/main_parts.kt
+++ app/src/main/java/com/example/proyect_b_line/main_parts.kt
@@ -1,14 +1,17 @@
 package com.example.proyect_b_line
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.runtime.*
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
 
 @Composable
 fun productCart(url: String){
@@ -22,16 +25,36 @@ fun productCart(url: String){
         shape = RoundedCornerShape(15.dp)
     ){
 
-    Column(verticalArrangement = Arrangement.Center, modifier = Modifier.padding(12.dp)){
+    Row( modifier = Modifier.padding(12.dp)){
             AsyncImage(model = url, contentDescription = "Hola texto", Modifier.requiredSize(125.dp))
+            contentCard(producDescrip = "", disponibility = true, exists = 0 , score = 0.0f)
         }
 
     }
 
 }
 
+@Composable
+fun contentCard(producDescrip:String, disponibility:Boolean, exists:Int, score:Float ){
+    Column(modifier = Modifier
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(text = "HOLA MUNDO")
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp)) {
+            Text(text = "Mas informacion del producto", fontSize = 9.sp)
+
+        }
+    }
+    
+}
+
 @Preview(showBackground = true)
 @Composable
 fun CardPreview() {
     productCart(url = "https://www.steren.com.gt/media/catalog/product/cache/b69086f136192bea7a4d681a8eaf533d/image/20986abca/audifonos-bluetooth-con-bateria-de-hasta-30-h.jpg")
+    //contentCard(producDescrip = "", disponibility = true, exists =0 , score =0.0f )
 }
