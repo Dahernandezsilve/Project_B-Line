2cfea352db5640838ce65a02cf932cf9bdae700b
Danval-003 <77825594+Danval-003@users.noreply.github.com>
2022-10-26 10:05:12 -0600

    Se arreglo el tamano del buscador

--- app/src/main/java/com/example/proyect_b_line/view/SearchView.kt
+++ app/src/main/java/com/example/proyect_b_line/view/SearchView.kt
@@ -1,15 +1,11 @@
 package com.example.proyect_b_line.view
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -25,35 +21,40 @@ fun SearchView() {
         var value by remember { mutableStateOf("") }
 
         ConstraintLayout(
-            modifier = Modifier.padding(5.dp, 10.dp).fillMaxWidth(),
+            modifier = Modifier.padding(0.dp, 10.dp).fillMaxWidth(),
         ) {
             val (searchTextField, buttonFilters) = createRefs()
             createEndBarrier(searchTextField,buttonFilters)
-            OutlinedTextField(value = value,
+
+            OutlinedTextField(singleLine = true, value = value,
                 onValueChange ={value=it},
+                maxLines = 1,
+                modifier = Modifier.size(300.dp, 50.dp).constrainAs(searchTextField){
+                    top.linkTo(parent.top, margin= 3.dp)
+                    bottom.linkTo(parent.bottom, margin = 3.dp)
+                    absoluteLeft.linkTo(parent.absoluteLeft, margin = 5.dp)
+                },
                 placeholder = {
                     Text(text = " Buscar",
                         color = Color.LightGray,
-                        style = MaterialTheme.typography.bodyLarge
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.fillMaxSize()
                     )
                 },
-                modifier = Modifier.constrainAs(searchTextField){
-                 top.linkTo(parent.top, margin = 3.dp)
-                 absoluteLeft.linkTo(parent.absoluteLeft, margin = 3.dp)
-                 absoluteRight.linkTo(buttonFilters.absoluteLeft, margin = 2.dp)
-                },
+
                 shape = RoundedCornerShape(200.dp),
-                textStyle = MaterialTheme.typography.bodyLarge,
+                textStyle = MaterialTheme.typography.bodyMedium,
                 leadingIcon = {
-                    Icon(Icons.Default.Search, contentDescription = "", modifier = Modifier.padding(5.dp))
+                    Icon(Icons.Default.Search, contentDescription = "", modifier = Modifier.padding(9.dp, 5.dp))
                 },
                 colors = TextFieldDefaults.textFieldColors(containerColor = MaterialTheme.colorScheme.secondary)
             )
             Button(modifier = Modifier
                 .size(70.dp, 40.dp)
                 .constrainAs(buttonFilters){
-                  top.linkTo(parent.top, margin= 9.dp)
-                  absoluteRight.linkTo(parent.absoluteRight, margin = 3.dp)
+                  top.linkTo(parent.top, margin= 3.dp)
+                  bottom.linkTo(parent.bottom, margin = 3.dp)
+                  absoluteRight.linkTo(parent.absoluteRight, margin = 5.dp)
                 }
                 , onClick = { /*TODO*/ },
                 colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
