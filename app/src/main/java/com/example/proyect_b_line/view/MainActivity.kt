363ca4f9088a2793ff865eeeccc85965480d7680
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-10-27 09:25:26 -0600

    Error mejorado

--- app/src/main/java/com/example/proyect_b_line/view/MainActivity.kt
+++ app/src/main/java/com/example/proyect_b_line/view/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.proyect_b_line.ui.theme.PB_Theme
+import com.example.proyect_b_line.view.screens.WishListScreen
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
