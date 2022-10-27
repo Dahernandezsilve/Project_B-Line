abcce47b3dc20838fc3a39096ad31642ff893f83
Diego Hernández <dahernandezsilvestre@gmail.com>
2022-10-27 09:18:13 -0600

    Optimización de importes y correción de errores

--- app/src/main/java/com/example/proyect_b_line/view/screens/WishListScreen.kt
+++ app/src/main/java/com/example/proyect_b_line/view/screens/WishListScreen.kt
@@ -1,27 +1,20 @@
-package com.example.proyect_b_line.view
+package com.example.proyect_b_line.view.screens
 
-import android.text.Layout.Alignment
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.proyect_b_line.ui.theme.PB_Theme
+import com.example.proyect_b_line.view.Stores
 
 @Composable
 fun WishListScreen (){
     PB_Theme {
-        Column() {
+        Column {
             Stores(listOf())
             ConstraintLayout(modifier = Modifier.fillMaxSize()) {
                 val welcomeText = createRef()
@@ -31,7 +24,7 @@ fun WishListScreen (){
                     absoluteRight.linkTo(parent.absoluteRight)
                     absoluteLeft.linkTo(parent.absoluteLeft)
 
-                }, textAlign = TextAlign.Center, text = " + Añade elementos para verlos aca!")
+                }, textAlign = TextAlign.Center, text = " + Añade elementos para verlos aquí!")
             }
         }
     }
