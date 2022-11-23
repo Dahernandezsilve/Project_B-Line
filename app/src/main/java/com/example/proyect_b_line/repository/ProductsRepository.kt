7509a1a2487f00c5e883b8bcbffcbd485371e2fa
Danval-003 <77825594+Danval-003@users.noreply.github.com>
2022-11-23 12:35:46 -0600

    splashscreen eliminado base de datos

--- app/src/main/java/com/example/proyect_b_line/repository/ProductsRepository.kt
+++ app/src/main/java/com/example/proyect_b_line/repository/ProductsRepository.kt
@@ -10,7 +10,7 @@ import kotlin.random.Random
 fun getProducts(): MutableList<Product>{
     return mutableListOf(
         Product(
-            Url="",
+            Url="https://www.steren.com.gt/media/catalog/product/cache/b69086f136192bea7a4d681a8eaf533d/image/20986abca/audifonos-bluetooth-con-bateria-de-hasta-30-h.jpg",
             urlImage = "https://www.steren.com.gt/media/catalog/product/cache/b69086f136192bea7a4d681a8eaf533d/image/20986abca/audifonos-bluetooth-con-bateria-de-hasta-30-h.jpg",
             "Audifonos L",
             false,
@@ -21,7 +21,7 @@ fun getProducts(): MutableList<Product>{
             13.0f,
         ),
         Product(
-            Url="",
+            Url="https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/41PSrh5lpdL._AC_SX466_.jpg",
             urlImage = "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/41PSrh5lpdL._AC_SX466_.jpg",
             "Joi Cons",
             false,
@@ -32,7 +32,7 @@ fun getProducts(): MutableList<Product>{
             50.0f,
         ),
         Product(
-            Url="",
+            Url="https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71lcyYiN9rL._AC_UY218_.jpg",
             urlImage = "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71lcyYiN9rL._AC_UY218_.jpg",
             "Nintendo Wii usada",
             true,
